9b08f091fe5f1e8c8554e869c42d81edb241d8b9
Marlene Moraes <marlenevmoraes@outlook.com>
2026-01-22 21:51:32 -0300

    feat: implement validation, CORS and Swagger documentation

--- src/candidates/dto/create-candidate.dto.ts
+++ src/candidates/dto/create-candidate.dto.ts
@@ -1,1 +1,35 @@
-export class CreateCandidateDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable prettier/prettier */
+import { IsEmail, IsNotEmpty, IsOptional, IsString, IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateCandidateDto {
+  @ApiProperty({ example: 'John Doe' })
+  @IsString()
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  fullName: string;
+
+  @ApiProperty({ example: 'joedoe@tech.com' })
+  @IsEmail({}, { message: 'E-mail inválido' })
+  email: string;
+
+  @ApiProperty({ example: '11999999999' })
+  @IsString()
+  @IsOptional()
+  phoneNumber?: string;
+
+  @ApiProperty({ example: 'https://linkedin.com/in/JohnDoe' })
+  @IsUrl()
+  @IsOptional()
+  linkedInUrl?: string;
+
+  @ApiProperty({ example: 'Backend' })
+  @IsString()
+  @IsNotEmpty()
+  role: string;
+
+  @ApiProperty({ example: 'Pleno' })
+  @IsString()
+  @IsNotEmpty()
+  experienceLevel: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('TechTalent Hub API')
+    .setDescription(
+      'API para gerenciamento de candidatos do Tech People Challenge',
+    )
+    .setVersion('1.0')
+    .addTag('candidates')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Acessível em localhost:3000/api
+
+  await app.listen(3000);
 }
 bootstrap();
